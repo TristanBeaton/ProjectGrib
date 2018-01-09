d70f6c4336673df6e2d89ca1d08939f1080710bd
= <tristanbeaton@outlook.co.nz>
2018-01-10 09:00:14 +1300

    Initial Commit

---
+++ ProjectGrib/ProjectGrib.h
@@ -0,0 +1,19 @@
+//
+//  ProjectGrib.h
+//  ProjectGrib
+//
+//  Created by Tristan Beaton on 10/01/18.
+//  Copyright © 2018 Tristan Beaton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProjectGrib.
+FOUNDATION_EXPORT double ProjectGribVersionNumber;
+
+//! Project version string for ProjectGrib.
+FOUNDATION_EXPORT const unsigned char ProjectGribVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectGrib/PublicHeader.h>
+
+
